536a415b891413ca956fcd0a1b588508376b05b2
VISCHER <david.rosenthal@vischer.com>
2026-02-13 00:42:30 +0100

    V120626 Update Added feature "InboxBoard" (Outlook), added feature "Draw.io Converter" to create web apps (Word)

--- Red Ink for Outlook/My Project/Settings.Designer.vb
+++ Red Ink for Outlook/My Project/Settings.Designer.vb
@@ -149,6 +149,18 @@ Namespace My
                 Me("selectedToolNames") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property InboxBoardColumns() As String
+            Get
+                Return CType(Me("InboxBoardColumns"),String)
+            End Get
+            Set
+                Me("InboxBoardColumns") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
