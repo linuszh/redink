13e0311eb93104513c938001bf2ad54df36ad3e8
VISCHER <david.rosenthal@vischer.com>
2026-04-05 23:59:44 +0200

    V050426 Documentation Updated documentation

--- Red Ink for Excel/ThisAddIn.vb
+++ Red Ink for Excel/ThisAddIn.vb
@@ -1,7 +1,7 @@
 ﻿' Part of "Red Ink for Excel"
 ' Copyright (c) LawDigital Ltd., Switzerland. All rights reserved. For license to use see https://redink.ai.
 '
-' 3.4.2026
+' 5.4.2026
 '
 ' The compiled version of Red Ink also ...
 '
@@ -56,7 +56,7 @@ Partial Public Class ThisAddIn
 
     ' Hardcoded config values
 
-    Public Shared Version As String = "V.030426" & SharedMethods.VersionQualifier
+    Public Shared Version As String = "V.050426" & SharedMethods.VersionQualifier
 
     Public Const AN As String = "Red Ink"
     Public Const AN2 As String = "redink"
